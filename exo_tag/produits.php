083296ecc9ef6719a63da4432373b8682ef3dc21
gwendoline <gwendoline.garlet+commit@live.fr>
2023-07-31 10:04:55 +0200

    add card detail

--- exo_tag/produits.php
+++ exo_tag/produits.php
@@ -23,14 +23,7 @@ require __DIR__ . '/methodes/function.php';
             </div>
         <?php }; ?>
     </div>
-    <?php
+<?php
 } else {
-    foreach ($_GET as $data) { ?>
-        <div class="card">
-            <h4> <?php echo $products[$data]['name']; ?> </h4>
-            <span> <?php echo 'Prix HT ' . $products[$data]['price']; ?> </span>
-            <span> <?php echo 'Prix TTC ' . priceTTC($products[$data]['price']); ?> </span>
-            <img src="<?php echo $products[$data]['img']; ?>" alt="<?php $products[$data]['img']; ?>">
-        </div>
-<?php };
+    require_once __DIR__ . '/layout/card-detail.php';
 }
